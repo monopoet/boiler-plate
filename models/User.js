af168c1acbcf4d919d6b7438a27dbd839805d8a9
monopoet <monopoet@naver.com>
2022-05-13 18:42:49 +0900

    added login,token,logout function

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -14,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50
+    maxlength: 100
   },
   role: {
     type: Number,
@@ -48,6 +49,43 @@ userSchema.pre('save', function( next ) {
 
 
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  // plainPassword 1234567 암호화된 비밀번호
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+  var user = this
+
+  // jsonwebtoken을 이용해서 token 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err);
+    cb(null, user)
+  })
+}
+
+userSchema.statics.findByToken = function ( token, cb ) {
+  var user = this;
+
+  // 토큰을 decode한다.
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    user.findOne({"_id" : decoded, "token" : token}, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user)
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
